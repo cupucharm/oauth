288a0571da4bdfaee9d8bdb00901da45239c287c
sujin <tnwlssl0729@naver.com>
2025-07-15 00:20:45 +0900

    jwt accesstoken 생성

--- src/main/java/com/example/oauth/member/controller/MemberController.java
+++ src/main/java/com/example/oauth/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.example.oauth.member.controller;
 
+import com.example.oauth.common.auth.JwtTokenProvider;
 import com.example.oauth.member.domain.Member;
 import com.example.oauth.member.dto.MemberCreateDto;
 import com.example.oauth.member.dto.MemberLoginDto;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * packageName    : com.example.oauth.member.controller
@@ -27,9 +31,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/member")
 public class MemberController {
     private final MemberService memberService;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public MemberController(MemberService memberService) {
+    public MemberController(MemberService memberService, JwtTokenProvider jwtTokenProvider) {
         this.memberService = memberService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @PostMapping("/create")
@@ -44,7 +50,12 @@ public class MemberController {
         Member member = memberService.login(memberLoginDto);
 
 //        일치할 경우 jwt accesstoken 생성
+        String jwtToken = jwtTokenProvider.createToken(member.getEmail(), member.getRole().toString());
 
-        return new ResponseEntity<>(member.getId(), HttpStatus.CREATED);
+        Map<String, Object> loginInfo = new HashMap<>();
+        loginInfo.put("id", member.getId());
+        loginInfo.put("token", jwtToken);
+
+        return new ResponseEntity<>(loginInfo, HttpStatus.OK);
     }
 }
