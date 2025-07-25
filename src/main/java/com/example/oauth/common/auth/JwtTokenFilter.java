3ceaa1ea2b0b5e60edc8f4a80a49f14591045486
sujin <tnwlssl0729@naver.com>
2025-07-25 23:02:34 +0900

    request, response 형변환 및 Bearer 토큰 검증

--- src/main/java/com/example/oauth/common/auth/JwtTokenFilter.java
+++ src/main/java/com/example/oauth/common/auth/JwtTokenFilter.java
@@ -1,6 +1,9 @@
 package com.example.oauth.common.auth;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -23,6 +26,15 @@ public class JwtTokenFilter extends GenericFilter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         // servletRequest 사용자의 모든 요청 값이 담겨있음
         // servletResponse을 통해서 사용자에게 응답을 줄 수 있음
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+        String token = httpServletRequest.getHeader("Authorization");
+        if (token != null) {
+            if(!token.substring(0, 7).equals("Bearer ")) {
+                throw new AuthenticationServiceException("Bearer 형식이 아닙니다.");
+            }
+        }
+
 
         // servletRequest 객체 안에서 토큰 꺼내기
 
