735e7e138c707658b9ad203b718c4249f7d38df8
sujin <tnwlssl0729@naver.com>
2025-07-29 21:56:46 +0900

    token 검증 및 claims(payload) 추출 예외처리

--- src/main/java/com/example/oauth/common/auth/JwtTokenFilter.java
+++ src/main/java/com/example/oauth/common/auth/JwtTokenFilter.java
@@ -8,6 +8,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -53,7 +56,8 @@ public class JwtTokenFilter extends GenericFilter {
             }
 
             // Authentication 객체 생성
-
+            Authentication authentication = new UsernamePasswordAuthenticationToken();
+            SecurityContextHolder.getContext().setAuthentication(authentication);
 
             // servletRequest 객체 안에서 토큰 꺼내기
 
